f68a2a4df1e402377be81c5faa5f801d81f10af4
dbolden92 <dominickbolden@gmail.com>
2021-10-25 20:10:44 -0600

    third commit

---
+++ action_page.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+
+Hello <?php echo $_POST["fname"];?><br>
+Your last name is <?php echo $_POST["lname"];?><br>
+Your email address is: <?php echo $_POST["email"];?><br>
+Your favorite sport is: <?php echo $_POST["sport"];?><br>
+Comments/Questions: <?php echo $_POST["subject"];?><br>
+
+
+
+</body>
+</html>
