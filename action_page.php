31e5b9f80e78d1a875b3e77d41426bd59a73fe19
dbolden92 <dominickbolden@gmail.com>
2021-11-03 16:47:35 -0600

    9th commit

--- action_page.php
+++ action_page.php
@@ -7,7 +7,5 @@ Your email address is: <?php echo $_POST["email"];?><br>
 Your favorite sport is: <?php echo $_POST["sport"];?><br>
 Comments/Questions: <?php echo $_POST["subject"];?><br>
 
-
-
 </body>
 </html>
